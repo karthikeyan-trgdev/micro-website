9cce8ea2368319062f6c766e9e9c7e695ebeb7cd
karthikeyan-trgdev <karthikeyan@toprockglobal.com>
2025-09-17 15:18:05 +0530

    event page

---
+++ Assets/script.js
@@ -0,0 +1,24 @@
+
+
+
+
+   $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive: {
+          0: {
+            items: 2,
+          },
+          600: {
+            items: 2,
+          },
+          1000: {
+            items: 2,
+          },
+        },
+      });
